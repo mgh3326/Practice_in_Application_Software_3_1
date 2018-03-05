1ec5b24ccb6be873188d07c31dc0dc699e53a742
mgh3326 <mgh3326@naver.com>
2018-03-05 09:58:03 +0900

    init

---
+++ 3_05_first/3_05_first/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3_05_first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
