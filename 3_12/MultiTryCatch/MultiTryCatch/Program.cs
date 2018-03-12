26e30996fc434a8c6a4b2226bd7145c35d7b3add
mgh3326 <mgh3326@naver.com>
2018-03-12 09:29:26 +0900

    update

---
+++ 3_12/MultiTryCatch/MultiTryCatch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 11, 22, 33 };
+            int[] arr2 = { 0, 1, 2 };
+            for (int i = 0; i < arr1.Length; ++i)
+            {
+                try
+                {
+                    Console.WriteLine(arr1[i] + "/" + arr2[i] + "=" + arr1[i] / arr2[i]);
+                }
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine("Cant't divide");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+        }
+    }
+}
