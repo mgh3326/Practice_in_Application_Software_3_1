bc63ddb4b9e27a77ec7ab8a1cfa8bb11ff8a7ecc
mgh3326 <mgh3326@naver.com>
2018-03-12 09:12:14 +0900

    update

---
+++ 3_12/Foreach/Foreach/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myAl = new ArrayList();
+            myAl.Add("Hello");
+            myAl.Add("World");
+            myAl.Add("!");
+            Console.Write("Values:");
+            PrintValues(myAl);
+        }
+
+        public static void PrintValues(ArrayList myList)
+        {
+            foreach(string text in myList)
+            {
+                Console.Write("\t{0}", text);
+            }
+            Console.WriteLine();
+        }
+    }
+}
