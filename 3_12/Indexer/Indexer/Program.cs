15b328bd2fe7a441ca903fab6f37bd3b7132565c
mgh3326 <mgh3326@naver.com>
2018-03-12 11:04:34 +0900

    update

---
+++ 3_12/Indexer/Indexer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Indexer
+{
+    class MyIndexerClass
+    {
+        private string[] data = new string[5];
+
+        public int Length
+        {
+            get { return data.Length; }
+        }
+        public string this[int index]
+        {
+            get { return data[index]; }
+            set { data[index] = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyIndexerClass mic = new MyIndexerClass();
+            mic[0] = "Seoul"; mic[1] = "Nowon"; mic[2] = "Kawngwoon"; mic[3] = "-road 20"; mic[4] = "Kwangwoon University";
+
+            for (int i = 0; i < mic.Length; i++)
+                Console.Write("{0} ", mic[i]);
+            Console.WriteLine();
+        }
+    }
+}
