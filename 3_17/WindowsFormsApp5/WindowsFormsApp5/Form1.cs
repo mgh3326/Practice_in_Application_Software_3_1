23be55a4922ac1b4d500255deca92f9234fba60c
mgh3326 <mgh3326@naver.com>
2018-03-19 11:23:03 +0900

    update

---
+++ 3_17/WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrClock_Tick(object sender, EventArgs e)
+        {
+            lblDisplay.Text = (int.Parse(lblDisplay.Text) + 1).ToString();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            tmrClock.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            tmrClock.Stop();
+        }
+    }
+}
