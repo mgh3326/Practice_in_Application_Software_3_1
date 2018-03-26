cb46d06aade3a6be9aa5a67453d5c104501d1d2a
mgh3326 <mgh3326@naver.com>
2018-03-26 11:05:49 +0900

    update

---
+++ 3_26/WindowsFormsApp5/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nms = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.dtpTime = new System.Windows.Forms.DateTimePicker();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.nms.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nms
+            // 
+            this.nms.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.nms.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem});
+            this.nms.Location = new System.Drawing.Point(0, 0);
+            this.nms.Name = "nms";
+            this.nms.Size = new System.Drawing.Size(337, 28);
+            this.nms.TabIndex = 0;
+            this.nms.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiOpen});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // tsmiOpen
+            // 
+            this.tsmiOpen.Name = "tsmiOpen";
+            this.tsmiOpen.Size = new System.Drawing.Size(216, 26);
+            this.tsmiOpen.Text = "열기(&O)";
+            this.tsmiOpen.Click += new System.EventHandler(this.tsmiOpen_Click);
+            // 
+            // dtpTime
+            // 
+            this.dtpTime.CustomFormat = "HH:mm:ss";
+            this.dtpTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpTime.Location = new System.Drawing.Point(227, 72);
+            this.dtpTime.Name = "dtpTime";
+            this.dtpTime.ShowUpDown = true;
+            this.dtpTime.Size = new System.Drawing.Size(98, 25);
+            this.dtpTime.TabIndex = 1;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.CustomFormat = "yyyy-MM-dd";
+            this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDate.Location = new System.Drawing.Point(24, 72);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(135, 25);
+            this.dtpDate.TabIndex = 2;
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(24, 103);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 3;
+            this.btnQuery.Text = "Query";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(136, 103);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(250, 103);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 130);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnQuery);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.dtpTime);
+            this.Controls.Add(this.nms);
+            this.MainMenuStrip = this.nms;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.nms.ResumeLayout(false);
+            this.nms.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip nms;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmiOpen;
+        private System.Windows.Forms.DateTimePicker dtpTime;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ 3_26/WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        List<String[]> awsList = new List<string[]>();
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeOpenFileDialog();
+            InitializeDataTimePicker();
+        }
+        private void InitializeOpenFileDialog()
+        {
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            ofd.Filter = "CSV (*.csv)|*.csv|" + "All files (*.*)|*.*";
+            ofd.FileName = "";
+        }
+        private void InitializeDataTimePicker()
+        {
+            dtpDate.Value = DateTime.ParseExact(DateTime.Now.ToString("yyyy-MM") + "-01", "yyy-MM-dd", null);
+            dtpTime.Value = DateTime.ParseExact("00:00:00", "HH:mm:ss", null);
+        }
+
+        private void tsmiOpen_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                awsList = readCSV(ofd.FileName);
+            }
+        }
+        private List<string[]> readCSV(string path)
+        {
+            StreamReader sr = new StreamReader(path);//using System.IO; 추가해줘야함
+            var csvList = new List<string[]>();
+            try
+            {
+                Text = Path.GetFileName(path);
+
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                    csvList.Add(line.Split(','));
+            }
+            catch (Exception ex)
+            {
+                Text = "";
+                MessageBox.Show(ex.Message, "File Open Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                sr.Close();
+            }
+            return csvList;
+        }
+        private DateTime GetDate()
+        {
+            string yyyyMMdd = dtpDate.Value.ToString("yyyy-MM-dd");
+            string HHmm = dtpTime.Value.ToString("HH:mm");
+
+            return DateTime.ParseExact(yyyyMMdd + ' ' + HHmm, "yyyy-MM-dd HH:mm", null);
+        }
+        private string GetTemperature()
+        {
+            string curDateString = GetDate().ToString("yyyy-MM-dd HH:mm");
+
+            for (int row = 0; row < awsList.Count; row++)
+            {
+                if (string.Equals(awsList[row][1], curDateString))
+                    return awsList[row][2];
+            }
+            return string.Empty;
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            if (ofd.FileName == string.Empty)
+            {
+                MessageBox.Show("입력된 파일이 없습니다.");
+                return;
+            }
+            string TemperatureString = GetTemperature();
+            if (string.Equals(TemperatureString, string.Empty))
+                MessageBox.Show(string.Format("{0}에는 자료가 없습니다.", GetDate()));
+            else
+                MessageBox.Show(string.Format("{0}에는 {1}도 입니다.", GetDate(), TemperatureString));
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (ofd.FileName == string.Empty)
+            {
+                MessageBox.Show("입력된 파일이 없습니다.");
+                return;
+            }
+            string FileName = Path.GetFileName(ofd.FileName);
+            StreamWriter sw = new StreamWriter(FileName, true);
+            sw.WriteLine(GetDate().ToString("yyyy-MM-dd HH:mm" + "," + GetTemperature()));
+            sw.Close();
+        }
+    }
+}
