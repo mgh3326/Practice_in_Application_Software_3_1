58b3372eaa223a9b4d4fde3bab21fa5a65a693e7
mgh3326 <mgh3326@naver.com>
2018-03-26 19:46:07 +0900

    update

--- 3_26/WindowsFormsApp6/WindowsFormsApp6/Form1.cs
+++ 3_26/WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.IO;
 namespace WindowsFormsApp6
 {
     public partial class Form1 : Form
@@ -14,6 +14,40 @@ namespace WindowsFormsApp6
         public Form1()
         {
             InitializeComponent();
+            initializeFileDialog();
         }
+        private void initializeFileDialog()
+        {
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            ofd.Filter = "Text (*.txt)|*.txt|" + "All files (*.*)|*.*";
+            ofd.FileName = "";
+
+            sfd.InitialDirectory = ofd.InitialDirectory;
+            sfd.Filter = ofd.Filter;
+            sfd.FileName = "*.txt";
+        }
+        private void tsmiNew_Click(object sender, EventArgs e)
+        {
+            Text = "Notepad";
+            txtNotepad.Text = string.Empty;
+        }
+
+        private void tsmiOpen_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Stream stream = ofd.OpenFile();
+                StreamReader sr = new StreamReader(stream);
+
+                txtNotepad.Text = sr.ReadToEnd();
+
+                sr.Close();
+                stream.Close();
+
+                Text = Path.GetFileName(ofd.FileName);
+
+            }
+        }
+
     }
 }
