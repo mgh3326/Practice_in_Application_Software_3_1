c443fe7ced2cfd9e31d6b5b2e8fafba90a977777
mgh3326 <mgh3326@naver.com>
2018-04-19 20:42:38 +0900

    update

--- 4_16/1_1_Chatting_program/1_1_Chatting_program/Form1.cs
+++ 4_16/1_1_Chatting_program/1_1_Chatting_program/Form1.cs
@@ -16,19 +16,24 @@ namespace _1_1_Chatting_program
 
     public partial class Form1 : Form
     {
-        public NetworkStream m_Stream;
-        public StreamReader m_Read;
-        public StreamWriter m_Write;
-        const int PORT = 2002;
-        private Thread m_ThReader;
+        // server client members 
+        public NetworkStream m_Stream;//네트워크 스트림 
+        public StreamReader m_Read;// 읽기  
+        public StreamWriter m_Write;//쓰기
+        const int PORT = 2002;//포트번호 
+        private Thread m_ThReader;//읽기 쓰레드
 
-        public bool m_bStop = false;
 
+        //server members 
+        public bool _bStop = false;//서버 시작&중단 플래그
         private TcpListener m_listener;
         private Thread m_thServer;
-
-        public bool m_bConnect = false;
+        //서버 작동 리스너 //서버 스레드
+        //client members
+        public bool m_bConnect = false;//서버 접속 플래그
         TcpClient m_Client;
+
+
 
         public Form1()
         {
