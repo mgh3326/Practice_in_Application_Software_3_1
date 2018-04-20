5048317997e4cdb31858c75471244f60a055d5a9
mgh3326 <mgh3326@naver.com>
2018-04-20 19:17:59 +0900

    update

--- 4_16/1_1_Chatting_program/1_1_Chatting_program/Form1.Designer.cs
+++ 4_16/1_1_Chatting_program/1_1_Chatting_program/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.txt_all = new System.Windows.Forms.TextBox();
-            this.txt_Send = new System.Windows.Forms.TextBox();
+            this.txt_send = new System.Windows.Forms.TextBox();
             this.btn_Server = new System.Windows.Forms.Button();
             this.btn_Connect = new System.Windows.Forms.Button();
             this.btn_exit = new System.Windows.Forms.Button();
@@ -47,12 +47,13 @@
             this.txt_all.Size = new System.Drawing.Size(188, 280);
             this.txt_all.TabIndex = 0;
             // 
-            // txt_Send
+            // txt_send
             // 
-            this.txt_Send.Location = new System.Drawing.Point(31, 342);
-            this.txt_Send.Name = "txt_Send";
-            this.txt_Send.Size = new System.Drawing.Size(100, 25);
-            this.txt_Send.TabIndex = 1;
+            this.txt_send.Location = new System.Drawing.Point(31, 342);
+            this.txt_send.Name = "txt_send";
+            this.txt_send.Size = new System.Drawing.Size(100, 25);
+            this.txt_send.TabIndex = 1;
+            this.txt_send.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_send_KeyDown);
             // 
             // btn_Server
             // 
@@ -62,16 +63,18 @@
             this.btn_Server.TabIndex = 2;
             this.btn_Server.Text = "서버 켜기";
             this.btn_Server.UseVisualStyleBackColor = true;
+            this.btn_Server.Click += new System.EventHandler(this.btn_Server_Click);
             // 
             // btn_Connect
             // 
-            this.btn_Connect.ForeColor = System.Drawing.Color.Crimson;
+            this.btn_Connect.ForeColor = System.Drawing.Color.Black;
             this.btn_Connect.Location = new System.Drawing.Point(26, 53);
             this.btn_Connect.Name = "btn_Connect";
             this.btn_Connect.Size = new System.Drawing.Size(138, 28);
             this.btn_Connect.TabIndex = 3;
-            this.btn_Connect.Text = "연결 끊기";
+            this.btn_Connect.Text = "서버 연결";
             this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
             // 
             // btn_exit
             // 
@@ -91,6 +94,7 @@
             this.btn_Send.TabIndex = 5;
             this.btn_Send.Text = "보내기";
             this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
             // 
             // txt_ServerIp
             // 
@@ -119,7 +123,7 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btn_Send);
             this.Controls.Add(this.btn_exit);
-            this.Controls.Add(this.txt_Send);
+            this.Controls.Add(this.txt_send);
             this.Controls.Add(this.txt_all);
             this.Name = "Form1";
             this.Text = "1:1 Chatting Program";
@@ -134,7 +138,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox txt_all;
-        private System.Windows.Forms.TextBox txt_Send;
+        private System.Windows.Forms.TextBox txt_send;
         private System.Windows.Forms.Button btn_Server;
         private System.Windows.Forms.Button btn_Connect;
         private System.Windows.Forms.Button btn_exit;
--- 4_16/1_1_Chatting_program/1_1_Chatting_program/Form1.cs
+++ 4_16/1_1_Chatting_program/1_1_Chatting_program/Form1.cs
@@ -25,10 +25,10 @@ namespace _1_1_Chatting_program
 
 
         //server members 
-        public bool _bStop = false;//서버 시작&중단 플래그
-        private TcpListener m_listener;
-        private Thread m_thServer;
-        //서버 작동 리스너 //서버 스레드
+        public bool m_bStop = false;//서버 시작&중단 플래그
+        private TcpListener m_listener;//서버 작동 리스너
+        private Thread m_thServer; //서버 스레드
+        
         //client members
         public bool m_bConnect = false;//서버 접속 플래그
         TcpClient m_Client;
@@ -42,8 +42,8 @@ namespace _1_1_Chatting_program
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            ServerStop();
-            DisConnect();
+            ServerStop();//서버 중지
+            Disconnect();//연결 종료
         }
 
         private void btn_exit_Click(object sender, EventArgs e)
@@ -62,8 +62,7 @@ namespace _1_1_Chatting_program
                 txt_all.AppendText(msg + "\n");
                 txt_all.Focus();
                 txt_all.ScrollToCaret();
-                txt_Send.Focus();
-
+                txt_send.Focus();
             }));
         }
         public void ServerStart()
@@ -79,8 +78,166 @@ namespace _1_1_Chatting_program
                 while (m_bStop)
                 {
                     TcpClient hClient = m_listener.AcceptTcpClient();
+                    if (hClient.Connected)
+                    {
+                        m_bConnect = true;
+                        Message("클라이언트 접속");
+
+                        m_Stream = hClient.GetStream();
+                        m_Read = new StreamReader(m_Stream);
+                        m_Write = new StreamWriter(m_Stream);
+
+                        m_ThReader = new Thread(new ThreadStart(Receive));
+                        m_ThReader.Start();
+                    }
                 }
             }
+            catch
+            {
+                Message("시작 도중에 오류 발생");
+                return;
+            }
+        }
+        public void ServerStop()
+        {
+            if (!m_bStop)
+                return;
+            m_listener.Stop();//서버 소켓 작동 중지
+
+            m_Read.Close();
+            m_Write.Close();
+
+            m_Stream.Close();
+
+            m_ThReader.Abort();//서버 소켓 스레드 종료
+            m_thServer.Abort();//스레드 종료
+
+            Message("서비스 종료");
+
+        }
+
+        public void Disconnect()
+        {
+            if (!m_bConnect)
+                return;
+            m_bConnect = false;
+
+            m_Read.Close();
+            m_Write.Close();
+
+            m_Stream.Close();
+            m_ThReader.Abort();
+
+            Message("상대방과 연결 중단");
+
+        }
+        public void Connect()
+        {
+            m_Client = new TcpClient();
+            try
+            {
+                m_Client.Connect(txt_ServerIp.Text, PORT);
+            }
+
+            catch
+            {
+                m_bConnect = false;
+                return;
+
+            }
+            m_bConnect = true;
+            Message("서버에 연결");
+            m_Stream = m_Client.GetStream();
+
+            m_Read = new StreamReader(m_Stream);
+            m_Write = new StreamWriter(m_Stream);
+            m_ThReader = new Thread(new ThreadStart(Receive));
+            m_ThReader.Start();
+
+        }
+        public void Receive()
+        {
+            try
+            {
+                while (m_bConnect)
+                {
+                    string szMessage = m_Read.ReadLine();
+                    if (szMessage != null)
+                        Message("상대방 >>> : " + szMessage);
+                }
+            }
+            catch
+            {
+                Message("데이터를 읽는 과정에서 오류가 발생");
+            }
+            Disconnect();
+        }
+        void Send()
+        {
+            try
+            {
+                m_Write.WriteLine(txt_send.Text);
+                m_Write.Flush();
+
+                Message(">>> : " + txt_send.Text);
+                txt_send.Text = "";
+            }
+            catch
+            {
+                Message("데이터 전송 실패");
+            }
+        }
+
+        private void btn_Server_Click(object sender, EventArgs e)
+        {
+            if (btn_Server.Text == "서버 켜기")
+            {
+                m_thServer = new Thread(new ThreadStart(ServerStart));
+                m_thServer.Start();
+                btn_Server.Text = "서버 멈춤";
+                btn_Server.ForeColor = Color.Red;
+
+            }
+            else
+            {
+                ServerStop();
+                btn_Server.Text = "서버 켜기";
+                btn_Server.ForeColor = Color.Black;
+            }
+
+
+
+
+        }
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            if (btn_Connect.Text == "서버 연결")
+            {
+                Connect();
+                if (m_bConnect)
+                {
+                    btn_Connect.Text = "연결 끊기";
+                    btn_Connect.ForeColor = Color.Red;
+                }
+                else
+                {
+                    Disconnect();
+                    btn_Connect.Text = "서버 연결";
+                    btn_Connect.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            Send();
+        }
+
+        private void txt_send_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                Send();
         }
     }
 }
