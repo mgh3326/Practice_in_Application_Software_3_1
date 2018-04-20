1ed6cda63198e6f12f2dce242c056d7ee99a1691
mgh3326 <mgh3326@naver.com>
2018-04-20 22:43:09 +0900

    complete

--- 4_2/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ 4_2/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,39 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.testFileBtn = new System.Windows.Forms.Button();
-            this.testDirBtn = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // testFileBtn
-            // 
-            this.testFileBtn.Location = new System.Drawing.Point(55, 55);
-            this.testFileBtn.Name = "testFileBtn";
-            this.testFileBtn.Size = new System.Drawing.Size(243, 70);
-            this.testFileBtn.TabIndex = 0;
-            this.testFileBtn.Text = "File.Exists (string filepath)";
-            this.testFileBtn.UseVisualStyleBackColor = true;
-            this.testFileBtn.Click += new System.EventHandler(this.testFileBtn_Click);
-            // 
-            // testDirBtn
-            // 
-            this.testDirBtn.Location = new System.Drawing.Point(55, 163);
-            this.testDirBtn.Name = "testDirBtn";
-            this.testDirBtn.Size = new System.Drawing.Size(243, 70);
-            this.testDirBtn.TabIndex = 1;
-            this.testDirBtn.Text = "Directory.Exists (string directorypath)";
-            this.testDirBtn.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.testDirBtn);
-            this.Controls.Add(this.testFileBtn);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
+			this.testFileBtn = new System.Windows.Forms.Button();
+			this.testDirBtn = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// testFileBtn
+			// 
+			this.testFileBtn.Location = new System.Drawing.Point(48, 44);
+			this.testFileBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.testFileBtn.Name = "testFileBtn";
+			this.testFileBtn.Size = new System.Drawing.Size(213, 56);
+			this.testFileBtn.TabIndex = 0;
+			this.testFileBtn.Text = "File.Exists (string filepath)";
+			this.testFileBtn.UseVisualStyleBackColor = true;
+			this.testFileBtn.Click += new System.EventHandler(this.testFileBtn_Click);
+			// 
+			// testDirBtn
+			// 
+			this.testDirBtn.Location = new System.Drawing.Point(48, 130);
+			this.testDirBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.testDirBtn.Name = "testDirBtn";
+			this.testDirBtn.Size = new System.Drawing.Size(213, 56);
+			this.testDirBtn.TabIndex = 1;
+			this.testDirBtn.Text = "Directory.Exists (string directorypath)";
+			this.testDirBtn.UseVisualStyleBackColor = true;
+			this.testDirBtn.Click += new System.EventHandler(this.testDirBtn_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(700, 360);
+			this.Controls.Add(this.testDirBtn);
+			this.Controls.Add(this.testFileBtn);
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
 
         }
 
--- 4_2/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ 4_2/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -3,22 +3,48 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void testFileBtn_Click(object sender, EventArgs e)
-        {
+		private void testFileBtn_Click(object sender, EventArgs e)
+		{
+			string file_path = "testFile.txt";
+			if (File.Exists(file_path))
+			{
+				File.Delete(file_path);
+				MessageBox.Show("파일을 삭제합니다.");
+			}
+			else
+			{
+				MessageBox.Show("파일이 존재 하지 않습니다.");
+			}
+		}
 
-        }
-    }
+		private void testDirBtn_Click(object sender, EventArgs e)
+		{
+			string directory_path = "testDirectory";
+			if (Directory.Exists(directory_path))
+			{
+				//true 로 설정하는 경로, 해당 디덕터리의 한뤼 디렉터리 및 파일들도 모두 삭제한다.
+				Directory.Delete(directory_path, true);
+				MessageBox.Show("디렉터리를 삭제합니다");
+			}
+			else
+			{
+
+				MessageBox.Show("디렉터리가 존재하지 않습니다.");
+			}
+		}
+	}
 }
