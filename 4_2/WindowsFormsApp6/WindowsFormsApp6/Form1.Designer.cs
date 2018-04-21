70d95e0e49cb12c9d11c03ebcfa352faa8bc36df
mgh3326 <mgh3326@naver.com>
2018-04-21 20:51:37 +0900

    update

--- 4_2/WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
+++ 4_2/WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
 			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
@@ -98,96 +97,12 @@
 			// 
 			this.cmuListView.ImageScalingSize = new System.Drawing.Size(20, 20);
 			this.cmuListView.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-=======
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
-            System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
-            System.Windows.Forms.ListViewGroup listViewGroup3 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.txtPath = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.cmuListView = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
-            this.mnuDetail = new System.Windows.Forms.ToolStripMenuItem();
-            this.mnuList = new System.Windows.Forms.ToolStripMenuItem();
-            this.mnuSmall = new System.Windows.Forms.ToolStripMenuItem();
-            this.mnuLarge = new System.Windows.Forms.ToolStripMenuItem();
-            this.imgList = new System.Windows.Forms.ImageList(this.components);
-            this.treeView1 = new System.Windows.Forms.TreeView();
-            this.lvwFiles = new System.Windows.Forms.ListView();
-            this.splitter1 = new System.Windows.Forms.Splitter();
-            this.trvDir = new System.Windows.Forms.TreeView();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.cmuListView.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.txtPath);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 62);
-            this.panel1.TabIndex = 0;
-            // 
-            // txtPath
-            // 
-            this.txtPath.Location = new System.Drawing.Point(108, 23);
-            this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(600, 25);
-            this.txtPath.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 34);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "전체경로";
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.listView1);
-            this.panel2.Controls.Add(this.treeView1);
-            this.panel2.Controls.Add(this.lvwFiles);
-            this.panel2.Controls.Add(this.splitter1);
-            this.panel2.Controls.Add(this.trvDir);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 62);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(800, 388);
-            this.panel2.TabIndex = 1;
-            // 
-            // listView1
-            // 
-            this.listView1.ContextMenuStrip = this.cmuListView;
-            this.listView1.LargeImageList = this.imgList;
-            this.listView1.Location = new System.Drawing.Point(124, 3);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(676, 385);
-            this.listView1.SmallImageList = this.imgList;
-            this.listView1.TabIndex = 4;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
-            // cmuListView
-            // 
-            this.cmuListView.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.cmuListView.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
->>>>>>> 213f15a1314fdfb59f6977b4f2b1f458b06fce85
             this.mnuOpen,
             this.toolStripMenuItem1,
             this.mnuDetail,
             this.mnuList,
             this.mnuSmall,
             this.mnuLarge});
-<<<<<<< HEAD
 			this.cmuListView.Name = "contextMenuStrip1";
 			this.cmuListView.Size = new System.Drawing.Size(139, 120);
 			// 
@@ -252,80 +167,6 @@
 			listViewGroup3.Header = "ListViewGroup";
 			listViewGroup3.Name = "colFileDate";
 			this.lvwFiles.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
-=======
-            this.cmuListView.Name = "contextMenuStrip1";
-            this.cmuListView.Size = new System.Drawing.Size(159, 130);
-            // 
-            // mnuOpen
-            // 
-            this.mnuOpen.Name = "mnuOpen";
-            this.mnuOpen.Size = new System.Drawing.Size(158, 24);
-            this.mnuOpen.Text = "열기";
-            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
-            // 
-            // toolStripMenuItem1
-            // 
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(155, 6);
-            // 
-            // mnuDetail
-            // 
-            this.mnuDetail.Name = "mnuDetail";
-            this.mnuDetail.Size = new System.Drawing.Size(158, 24);
-            this.mnuDetail.Text = "자세히";
-            this.mnuDetail.Click += new System.EventHandler(this.mnuView_Click);
-            // 
-            // mnuList
-            // 
-            this.mnuList.Name = "mnuList";
-            this.mnuList.Size = new System.Drawing.Size(158, 24);
-            this.mnuList.Text = "간단히";
-            this.mnuList.Click += new System.EventHandler(this.mnuView_Click);
-            // 
-            // mnuSmall
-            // 
-            this.mnuSmall.Name = "mnuSmall";
-            this.mnuSmall.Size = new System.Drawing.Size(158, 24);
-            this.mnuSmall.Text = "작은 아이콘";
-            this.mnuSmall.Click += new System.EventHandler(this.mnuView_Click);
-            // 
-            // mnuLarge
-            // 
-            this.mnuLarge.Name = "mnuLarge";
-            this.mnuLarge.Size = new System.Drawing.Size(158, 24);
-            this.mnuLarge.Text = "큰 아이콘";
-            this.mnuLarge.Click += new System.EventHandler(this.mnuView_Click);
-            // 
-            // imgList
-            // 
-            this.imgList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgList.ImageStream")));
-            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
-            this.imgList.Images.SetKeyName(0, "20171214_164123.png");
-            this.imgList.Images.SetKeyName(1, "20171214_164624.png");
-            this.imgList.Images.SetKeyName(2, "introduce1.png");
-            this.imgList.Images.SetKeyName(3, "스크린샷(2).png");
-            // 
-            // treeView1
-            // 
-            this.treeView1.ImageIndex = 0;
-            this.treeView1.ImageList = this.imgList;
-            this.treeView1.Location = new System.Drawing.Point(0, 3);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.SelectedImageIndex = 0;
-            this.treeView1.Size = new System.Drawing.Size(121, 385);
-            this.treeView1.TabIndex = 3;
-            // 
-            // lvwFiles
-            // 
-            this.lvwFiles.Dock = System.Windows.Forms.DockStyle.Fill;
-            listViewGroup1.Header = "ListViewGroup";
-            listViewGroup1.Name = "colFileName";
-            listViewGroup2.Header = "ListViewGroup";
-            listViewGroup2.Name = "colFileSize";
-            listViewGroup3.Header = "ListViewGroup";
-            listViewGroup3.Name = "colFileDate";
-            this.lvwFiles.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
->>>>>>> 213f15a1314fdfb59f6977b4f2b1f458b06fce85
             listViewGroup1,
             listViewGroup2,
             listViewGroup3});
