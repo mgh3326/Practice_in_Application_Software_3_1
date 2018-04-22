c8dae85b889054c31b6dd783c90961e6cbb82189
mgh3326 <mgh3326@naver.com>
2018-04-22 19:17:02 +0900

    update

---
+++ 4_16/Packet_server/Packet_server/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Packet_server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_server_state = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_server_state
+            // 
+            this.txt_server_state.Location = new System.Drawing.Point(13, 13);
+            this.txt_server_state.Multiline = true;
+            this.txt_server_state.Name = "txt_server_state";
+            this.txt_server_state.Size = new System.Drawing.Size(673, 398);
+            this.txt_server_state.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_server_state);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_server_state;
+    }
+}
+
