b033b22b5160e6a8688bb05b2f17f6ef2de14963
mgh3326 <mgh3326@naver.com>
2018-04-22 19:28:21 +0900

    update

---
+++ 4_16/Packet_server/Packet_server/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+using System.Net.Sockets;
+using System.Threading;
+namespace Packet_server
+{
+    partial class Form1
+    {
+        private NetworkStream m_networkstream;
+        private TcpListener m_listener;
+
+        private byte[] sendBuffer = new byte[1024 * 4];
+        private byte[] readBuffer = new byte[1024 * 4];
+
+        private bool m_bClientOn = false;
+
+        public Initialize m_initializeClass;
+        public Login m_loginClass;
+
+        public void RUN()
+        {
+            this.m_listener = new TcpListener(7777);
+            this.m_listener.Start();
+
+            if (!this.m_bClientOn)
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    this.txt_server_state.AppendText("클라이언트 접속 대기중\n");
+                }));
+            }
+            TcpClient client = this.m_listener.AcceptTcpClient();
+            if (client.Connected)
+            {
+                this.m_bClientOn = true;
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    this.txt_server_state.AppendText("클라이언트 접속\n");
+                }));
+                m_networkstream = client.GetStream();
+            }
+
+            int nRead = 0;
+
+        }
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_server_state = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_server_state
+            // 
+            this.txt_server_state.Location = new System.Drawing.Point(33, 35);
+            this.txt_server_state.Multiline = true;
+            this.txt_server_state.Name = "txt_server_state";
+            this.txt_server_state.Size = new System.Drawing.Size(714, 376);
+            this.txt_server_state.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_server_state);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_server_state;
+    }
+}
+
---
+++ 4_16/Packet_server/Packet_server/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Packet_server
+{
+    static class Program
+    {
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
