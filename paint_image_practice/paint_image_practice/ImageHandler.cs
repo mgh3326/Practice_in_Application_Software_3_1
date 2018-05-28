9aa711ceb3d06d47af2b4f64ec59ac2f10f399a4
mgh3326 <mgh3326@naver.com>
2018-05-28 09:05:37 +0900

    imagehandler update

---
+++ paint_image_practice/paint_image_practice/ImageHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paint_image_practice
+{
+    public class ImageHandler
+    {
+        private string _bitmapPath;
+        private Bitmap _CurrentBitmap;
+
+        public Bitmap CurrentBitmap
+        {
+            get
+            {
+                if (_CurrentBitmap == null)
+
+                    _CurrentBitmap = new Bitmap(1, 1);
+                return _CurrentBitmap;
+
+            }
+            set
+            {
+                _CurrentBitmap = value;
+            }
+        }
+
+
+        public string BitmapPath
+        {
+            get
+            {
+                return _bitmapPath;
+            }
+
+
+            set
+            {
+                _bitmapPath = value;
+            }
+        }
+    }
+}
